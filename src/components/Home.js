12e09c84559c6c93c93fe3e709d74322d9a47273
Akanksha☺️ <89179275+Enthusiast22@users.noreply.github.com>
2021-10-16 00:33:13 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <h1 align="center">This is the HOME PAGE</h1>
+            <h1 align="center">This is the HOME PAGE!!!</h1>
         </div>
     )
 }
